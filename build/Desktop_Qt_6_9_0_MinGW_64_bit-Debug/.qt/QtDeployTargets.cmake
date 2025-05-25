f39c7c7d2eaba2bbe9b04e74c7fac50adc32053f
iceisnicehq <iceisnicehq@gmail.com>
2025-05-25 04:51:17 +0300

    init

--- build/Desktop_Qt_6_9_0_MinGW_64_bit-Debug/.qt/QtDeployTargets.cmake
+++
@@ -1,3 +0,0 @@
-set(__QT_DEPLOY_TARGET_QT_note_FILE C:/Users/Ice/QT_KR/QT_note/QT_note/build/Desktop_Qt_6_9_0_MinGW_64_bit-Debug/QT_note.exe)
-set(__QT_DEPLOY_TARGET_QT_note_TYPE EXECUTABLE)
-set(__QT_DEPLOY_TARGET_QT_note_RUNTIME_DLLS C:/Qt/6.9.0/mingw_64/bin/Qt6Widgets.dll;C:/Qt/6.9.0/mingw_64/bin/Qt6Gui.dll;C:/Qt/6.9.0/mingw_64/bin/Qt6Core.dll)
